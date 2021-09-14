da2466691e3eca9e7d51ec055a0bcb2963949bc6
bjpogs <bjpogs26@gmail.com>
2021-09-14 08:48:15 +0800

    server

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://7c24-112-198-179-122.ngrok.io/api/v1/users/'
+    baseURL: 'http://fa40-112-198-179-122.ngrok.io/api/v1/users/'
 })
