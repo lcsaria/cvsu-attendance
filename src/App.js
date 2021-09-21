7793ffd7132dc76d0845d946d0986c1e99c08cc0
lcsaria <lmarlsaria21@gmail.com>
2021-09-21 17:37:06 +0800

    update 92121

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import RouteTree from '../src/components/routes/RouteTree';
 import "./assets/bootstrap/css/bootstrap.min.css"
 import "./assets/css/sidebar.css"
 import "./assets/css/content.css"
+import "react-datepicker/dist/react-datepicker.css";
 import { Modal } from 'react-bootstrap';
 
 function App() {
