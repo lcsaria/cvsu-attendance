580875346f675bcb278ba5e619ef5b8de406a676
lcsaria <lmarlsaria21@gmail.com>
2021-09-28 10:25:58 +0800

    fixed bug - footer

--- src/components/template/Footer.js
+++ src/components/template/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <footer className="bg-white sticky-footer">
-        <div className="container my-auto">
+        <div className="mt-2">
           <div className="text-center my-auto copyright">
             <span>Copyright © CvSU GenTri 2021</span>
           </div>
